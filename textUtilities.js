a253676f3ba7e5ce27c135dc19b683b202e9307f
willowcheng <e.wingus@gmail.com>
2017-03-16 16:48:05 -0400

    Red, green and refactor

---
+++ textUtilities.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+expect(true).to.be.true;
+
+function titleCase (title) {
+    return title;
+}
+
+expect(titleCase('the great mouse detective')).to.be.a('string');
