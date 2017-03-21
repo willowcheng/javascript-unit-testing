3768a45ff44c4ff24b9ed65da16d453d037a41bc
willowcheng <e.wingus@gmail.com>
2017-03-21 15:05:36 -0400

    Test suite

--- test/ship_test.js
+++ test/ship_test.js
@@ -15,4 +15,54 @@ describe('checkForShip', () => {
 
     expect(checkForShip(player, [9, 9])).to.be.false;
   })
+
+  it('should correctly report a ship located at the given coordinates', () => {
+
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    }
+
+    expect(checkForShip(player, [0, 0])).to.be.true;
+  })
+
+  it('should handle ships located at more than one coordinate', () => {
+
+    player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  })
+
+  it('should handle checking multiple ships', () => {
+
+    player = {
+      ships: [
+        {
+          locations: [[0, 0], [0, 1]]
+        },
+        {
+          locations: [[1, 0], [1, 1]]
+        },
+        {
+          locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [1, 0])).to.be.true;
+    expect(checkForShip(player, [1, 1])).to.be.true;
+    expect(checkForShip(player, [2, 3])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  })
 })
