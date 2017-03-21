1c919df25a152e9545491f45ad43e6fb4f843a36
willowcheng <e.wingus@gmail.com>
2017-03-21 14:20:34 -0400

    Test suites and test specs (describe and it)

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+// Test suite
+describe('Mocha', () => {
+  // Test spec (unit test)
+  it('should run our tests using npm', () => {
+    expect(true).to.be.ok;
+  });
+});
