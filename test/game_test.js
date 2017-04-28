a4b8dc64cf4a5f02c2dc5af7c54d7f4b13379e85
willowcheng <e.wingus@gmail.com>
2017-04-28 17:36:07 -0400

    Mocks and stubs

--- test/game_test.js
+++ test/game_test.js
@@ -1,9 +1,45 @@
-var expect = require('chai').expect
+var expect = require('chai').expect;
 
-describe('GAME INSTANCE FUNCTIONS ', function () {
+describe('GAME INSTANCE FUNCTIONS', function () {
   describe('checkGameStatus', function () {
-    it('should tell me when the game is over')
-  })
+    var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+    it('should tell me when the game is over', function () {
+      var players = [
+        {
+					ships: [
+						{
+							locations: [[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    });
+  });
 
-  it('some other stuff')
-})
+  describe('takeTurn', function () {
+    var takeTurn = require('../game_logic/game_instance').takeTurn;
+    var guess, player;
+
+    beforeEach(function () {
+      guess = function () {
+        return [0, 0];
+      };
+      player = {
+        ships: [
+          {
+            locations: [[0, 0]],
+            damage: []
+          }
+        ]
+      }
+    });
+
+    it('should return false if the game ends', function () {
+      var actual = takeTurn(player, guess);
+      expect(actual).to.be.false;
+    });
+  });
+});
