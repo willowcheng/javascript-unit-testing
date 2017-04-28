08d9473df9d18d1759923fe4fb4b8cfb2f5a6214
willowcheng <e.wingus@gmail.com>
2017-04-28 17:48:45 -0400

    Test asynchronous code

--- test/game_test.js
+++ test/game_test.js
@@ -42,4 +42,21 @@ describe('GAME INSTANCE FUNCTIONS', function () {
       expect(actual).to.be.false;
     });
   });
+
+  function saveGame (callback) {
+    setTimeout(function () {
+      callback()
+    }, 1000)
+  }
+
+  describe('saveGame', function () {
+    it('should update save status', function (done) {
+      var status = 'game not saved...';
+      saveGame(function () {
+        status = 'game saved!';
+        expect(status).to.equal('game saved!');
+        done();
+      });
+    })
+  })
 });
